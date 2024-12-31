18d2884a1b02c81bc835e8dfa4e5673682fd028b
Bartosz <holzturbs@gmail.com>
2024-12-31 09:17:12 +0100

    Delete unnessesery line.

--- src/components/organisms/TravelNavbar.vue
+++ src/components/organisms/TravelNavbar.vue
@@ -1,13 +1,12 @@
 <template>
     <MobileMenu v-if="isMobile" />
     <DesktopMenu v-else />
-    {{windowWidth}}
 </template>
 <script setup>
 import DesktopMenu from "../molecules/DesktopMenu.vue";
 import MobileMenu from "../molecules/MobileMenu.vue";
 import useWindowWidth from "@/comosables/useWindowWidth";
 
-const { windowWidth, isMobile } = useWindowWidth();
+const { isMobile } = useWindowWidth();
 </script>
 <style scoped></style>
