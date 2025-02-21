1a2ad3360d02919973c4d7049511942367a4cf85
Bartosz <holzturbs@gmail.com>
2025-02-21 15:39:37 +0100

    Create new file SocialMedia.vue

---
+++ src/components/molecules/SocialMedia.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="SocialMedia">
+        <a href="https://www.instagram.com/">
+            <Instagram class="icoInstagram" />
+        </a>
+        <a href="https://www.youtube.com/">
+            <Youtube class="icoYouTube" />
+        </a>
+        <a href="https://www.facebook.com/">
+            <Facebook class="icoFacebook" />
+        </a>
+    </div>
+</template>
+<script setup>
+import { Instagram, Youtube, Facebook } from 'lucide-vue-next';
+</script>
+<style scoped></style>
