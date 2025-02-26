983ddf06b1574d120417e98b5d08f74a499fe1c8
Bartosz <holzturbs@gmail.com>
2025-02-26 13:58:27 +0100

    Change color icon and style in SocialMedia.vue

--- src/components/molecules/SocialMedia.vue
+++ src/components/molecules/SocialMedia.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="SocialMedia">
         <a class="SocialMediaIcon" href="https://www.instagram.com/">
-            <Instagram :size="35" color="#000000" class="icoInstagram" />
+            <Instagram :size="35" color="#000000d8" class="icoInstagram" />
         </a>
         <a class="SocialMediaIcon" href="https://www.youtube.com/">
-            <Youtube :size="35" color="#000000" class="icoYouTube" />
+            <Youtube :size="35" color="#000000d8" class="icoYouTube" />
         </a>
         <a class="SocialMediaIcon" href="https://www.facebook.com/">
-            <Facebook :size="35" color="#000000" class="icoFacebook" />
+            <Facebook :size="35" color="#000000d8" class="icoFacebook" />
         </a>
     </div>
 </template>
@@ -22,8 +22,10 @@ import { Instagram, Youtube, Facebook } from 'lucide-vue-next';
 
 
 .SocialMediaIcon {
-    background-color: #fff;
-    padding: 15px;
+    align-items: center;
+    justify-content: center;
+    background-color: #ffffffe8;
+    padding: 5px;
     border-radius: 8px;
 }
 </style>
