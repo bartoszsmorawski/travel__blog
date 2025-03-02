8aceade61105ebf04c2ce7ba21a949c5dc85a2c7
Bartosz <holzturbs@gmail.com>
2025-03-02 16:06:43 +0100

    Added new style (title, description) in FotoSectionBaner.vue

--- src/components/organisms/FotoSectionBaner.vue
+++ src/components/organisms/FotoSectionBaner.vue
@@ -20,6 +20,20 @@ const backgroundImageValue = computed(() => `url("${props.srcImg}")`);
 
 </script>
 <style scoped>
+.Title {
+    color: #e9e9e8;
+    font-size: 30px;
+    font-weight: 500;
+    margin-bottom: 20px;
+}
+
+.Description {
+    color: #e9e9e8;
+    font-size: 18px;
+    font-weight: 300;
+    margin-bottom: 20px;
+    line-height: 1.4;
+}
 .FotoBaner {
     color: #F5F5F4;
     text-align: center;
@@ -39,7 +53,7 @@ const backgroundImageValue = computed(() => `url("${props.srcImg}")`);
     height: 100%;
     top: 0px;
     left: 0px;
-    background-color: #00000071;
+    background-color: #00000089;
     z-index: 1;
 }
 
