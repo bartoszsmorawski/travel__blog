5ef4a9701681114635a14ef063ef2e7db5122f5a
Bartosz <holzturbs@gmail.com>
2025-03-15 15:41:46 +0100

    Added information about Austria to CountriesArticleView.vue

--- src/pages/CountriesArticleView.vue
+++ src/pages/CountriesArticleView.vue
@@ -1,5 +1,184 @@
-<template>Austria....itd poszczegolny article o danym kraju z podmenu. Podstawić tylko inny tekst.</template>
+<template>
+  <TravelNavbar backgroundgcColor="#4A4A4A" />
+  <div class="article-page">
+    <h1 class="title">- Porozmawiajmy o Austrii -</h1>
+    <h2 class="subTitle">Czyli co warto wiedzieć o tym kraju ?</h2>
+
+    <div class="article-container">
+      <img class="article-img" src="@/assets/image/wpis1.jpg">
+      <div>
+        <h3 class="article-title">Atrakcje turystyczne Wiednia. Top 9 miejsc, które Cię zachwycą</h3>
+        <p class="article-content">Wiedeń, co warto zobaczyć odwiedzając to miasto na weekend Wiedeń atrakcje
+          turystyczne-
+          czyli dzisiaj o tym co
+          warto zobaczyć w Wiedniu, wybierając się tam na weekend. Wiedeń : stolica i jednocześnie największe miasto w
+          Austrii. Liczące około 1.8 mln ludzi miasto jest jednym z najchętniej odwiedzanych miast w Europie. Centrum
+          muzyki
+          klasycznej : to tu tworzyli Mozart, Beethoven, Schubert i wielu innym wirtuozów. Wiedeń przez niektórych
+          nazywany
+          „Miastem Marzeń” bo...</p>
+        <a href="#" class="read-more">Czytaj dalej »</a>
+      </div>
+    </div>
+
+    <div class="article-container">
+      <img class="article-img" src="@/assets/image/wpis2.jpg">
+      <div>
+        <h3 class="article-title">Salzburg : miasto Mozarta, historii i bajkowych widoków</h3>
+        <p class="article-content">Salzburg to jedno z najpiękniejszych miast Austrii, położone u podnóża Alp, nad rzeką
+          Salzach. Znane jako miejsce narodzin Wolfganga Amadeusza Mozarta, łączy w sobie bogatą historię, kulturę oraz
+          wyjątkową architekturę.Historia Salzburga sięga czasów rzymskich, kiedy funkcjonowało tu miasto Juvavum. W
+          średniowieczu stał się ważnym ośrodkiem religijnym i handlowym, a swoje bogactwo zawdzięczał wydobyciu soli,
+          nazywanej „białym złotem”. To...</p>
+        <a href="#" class="read-more">Czytaj dalej »</a>
+      </div>
+    </div>
+
+    <div class="article-info">
+      <h2>Podróżowanie po Austrii</h2>
+      <p>Austria ma doskonale rozwiniętą sieć transportu. Koleje ÖBB oferują wygodne połączenia między miastami, a
+        autostrady i drogi są w bardzo dobrym stanie. Warto też skorzystać z lokalnych środków transportu, takich jak
+        tramwaje w Wiedniu czy kolejki górskie w Alpach.
+      </p>
+
+      <h2>Austria : kraina Alp, zabytków i niezwykłych podróży</h2>
+      <p>Austria to kraj pełen kontrastów : od majestatycznych szczytów Alp po historyczne miasta pełne zabytków i
+        kultury.
+        To idealne miejsce na wyjazd zarówno dla miłośników górskich wędrówek, sportów zimowych, jak i pasjonatów
+        historii
+        oraz muzyki. Dzięki doskonale rozwiniętej infrastrukturze turystycznej Austria oferuje podróżnym komfortowe
+        warunki
+        do zwiedzania i wypoczynku przez cały rok.</p>
+
+      <h3>Wiedeń : stolica pełna historii i kultury</h3>
+      <p>Wiedeń, stolica Austrii, to miasto o bogatej historii i niezwykłej atmosferze. Zachwyca elegancką architekturą,
+        pięknymi pałacami i licznymi atrakcjami kulturalnymi. Do najważniejszych zabytków należą:</p>
+      <ul>
+        <li>Pałac Schönbrunn : dawna letnia rezydencja Habsburgów, otoczona przepięknymi ogrodami,</li>
+        <li>Hofburg : monumentalny kompleks pałacowy, który przez wieki był centrum władzy cesarskiej,</li>
+        <li>Katedra św. Szczepana : gotycka perła Wiednia, symbol miasta,</li>
+        <li>Opera Wiedeńska : jedno z najważniejszych miejsc na muzycznej mapie świata,</li>
+        <li>Muzeum Historii Sztuki i Muzeum Albertina : skarbnice dzieł światowej klasy artystów.</li>
+      </ul>
+      <p>Wiedeń słynie również z kawiarni i tradycyjnych specjałów kulinarnych, takich jak tort Sachera czy Wiener
+        Schnitzel. To idealne miejsce na spacer po urokliwych uliczkach i odpoczynek w parkach, jak np. Prater czy
+        Burggarten.</p>
+
+      <h3>Salzburg : miasto Mozarta</h3>
+      <p>Położony u stóp Alp Salzburg to miasto, w którym urodził się Wolfgang Amadeusz Mozart. Jego zabytkowe Stare
+        Miasto
+        znajduje się na liście UNESCO. Warto zobaczyć:</p>
+      <ul>
+        <li>Twierdzę Hohensalzburg : imponującą warownię górującą nad miastem,</li>
+        <li>Dom Mozarta : muzeum poświęcone słynnemu kompozytorowi,</li>
+        <li>Pałac i ogrody Mirabell : niezwykle malownicze miejsce, idealne na spacer,</li>
+        <li>Katedrę św. Ruperta : jeden z najważniejszych kościołów w Austrii.</li>
+      </ul>
+      <p>Salzburg to także miasto festiwali : co roku odbywa się tu słynny Festiwal Salzburski, przyciągający miłośników
+        muzyki klasycznej z całego świata.</p>
+
+      <h3>Alpy austriackie : raj dla miłośników natury i sportów zimowych</h3>
+      <p>Austria to prawdziwy raj dla osób kochających góry. Alpy austriackie oferują doskonałe warunki do uprawiania
+        narciarstwa, snowboardu, turystyki pieszej i wspinaczki. Najpopularniejsze regiony to:</p>
+      <ul>
+        <li>Tyrol : region słynący z malowniczych dolin i doskonałych stoków narciarskich w miejscowościach takich jak
+          Innsbruck, Kitzbühel czy Sölden,</li>
+        <li>Zell am See-Kaprun : obszar idealny dla miłośników sportów wodnych i górskich wędrówek,</li>
+        <li>Dolina Wachau : znana z winnic i rejsów po Dunaju, doskonałe miejsce na relaksującą podróż.</li>
+      </ul>
+      <p>Latem Alpy zachęcają do wędrówek, jazdy na rowerze oraz podziwiania zapierających dech w piersiach widoków.
+        Zimą
+        Austria staje się mekką narciarzy z całego świata, oferując świetnie przygotowane trasy i nowoczesne kurorty.
+      </p>
+
+      <h3>Inne ciekawe miejsca w Austrii</h3>
+      <ul>
+        <li>Hallstatt : jedno z najpiękniejszych miasteczek w Europie, położone nad jeziorem i otoczone górami. To
+          miejsce
+          wpisane na listę UNESCO przyciąga turystów swoim bajkowym wyglądem.</li>
+        <li>Graz : miasto pełne zabytków, znane z urokliwego starego miasta i współczesnej architektury, np.
+          futurystycznego
+          budynku Kunsthaus Graz.</li>
+        <li>Innsbruck : stolica Tyrolu, łącząca historię z nowoczesnością, idealna baza wypadowa dla narciarzy i
+          miłośników
+          gór.</li>
+        <li>Krimml : wodospady Krimml : jedne z najwyższych w Europie, spektakularne miejsce dla fanów przyrody.</li>
+      </ul>
+
+      <h3>Podsumowanie</h3>
+      <p>Austria to kraj pełen niesamowitych atrakcji, łączący piękno przyrody z bogatą historią i kulturą. Niezależnie
+        od
+        pory roku można tu znaleźć coś dla siebie : od zwiedzania zabytkowych miast, przez relaks w malowniczych
+        dolinach,
+        aż po aktywne wakacje w Alpach. To jedno z tych miejsc, do których chce się wracać, by odkrywać jego kolejne
+        tajemnice.</p>
+    </div>
+  </div>
+</template>
 
 <script setup>
+import TravelNavbar from '@/components/organisms/TravelNavbar.vue';
+
 
 </script>
+<style scoped>
+.article-page {
+  margin-top: 80px;
+  padding-left: 60px;
+  padding-right: 60px;
+}
+
+.title {
+  text-align: center;
+  font-size: 40px;
+  font-weight: 500;
+  margin-top: 150px;
+  margin-bottom: 10px;
+}
+
+.subTitle {
+  text-align: center;
+  font-size: 22px;
+  font-weight: 300;
+  margin-top: 15px;
+}
+
+.article-container {
+  display: flex;
+  align-items: center;
+  gap: 50px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 16px;
+  margin-top: 80px;
+  margin-bottom: 20px;
+}
+
+.article-img {
+  width: 30%;
+  border-radius: 8px;
+}
+
+.article-title {
+  color: #333;
+  text-decoration: none;
+  font-size: 1.5rem;
+}
+
+.article-content {
+  font-size: 1rem;
+  color: #333;
+}
+
+.read-more {
+  color: #333;
+  text-decoration: none;
+  font-weight: bold;
+  font-size: 1.1rem;
+}
+
+.article-info {
+  padding: 16px;
+}
+
+</style>
