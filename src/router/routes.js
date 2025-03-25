a37e784a7cbfb18f4eae84f1440b5dc1258ef17d
Bartosz <holzturbs@gmail.com>
2025-03-25 10:54:09 +0100

    Create basic api
    Co-authored-by: Maty2002v2 <Maty2002v2@users.noreply.github.com>

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,22 +1,23 @@
 import HomeView from '../pages/HomeView.vue';
 import CountriesView from '../pages/CountriesView.vue';
-import CountriesArticleView from '../pages/CountriesArticleView.vue';
+import CountryView from '../pages/CountryView.vue';
+// import CountriesArticleView from '../pages/CountriesArticleView.vue';
 import BlogView from '../pages/BlogView.vue';
 import AboutUsView from '../pages/AboutUsView.vue';
 import TravelView from '../pages/TravelView.vue';
 import ShopView from '../pages/ShopView.vue';
 
 const routes = [
-	{ path: '/', component: HomeView },
-	{ path: '/wycieczki', component: TravelView },
-	{ path: '/sklep', component: ShopView },
-	{ path: '/kraje', component: CountriesView },
-	{ path: '/npAustria', component: CountriesArticleView },
-	{ path: '/blog', component: BlogView },
-	{ path: '/kraje', component: CountriesView },
-	{ path: '/onas', component: AboutUsView },
-
+	{ path: '/', component: HomeView, meta: { title: 'Strona główna' } },
+	{ path: '/wycieczki', component: TravelView, meta: { title: 'Wycieczki' } },
+	{ path: '/sklep', component: ShopView, meta: { title: 'Sklep' } },
+	{ path: '/kraje', component: CountriesView,  meta: { title: 'Kraje' } },
+	{ path: '/kraje/:country', component: CountryView},
+	{ path: '/blog', component: BlogView, meta: { title: 'Blog' } },
+	{ path: '/onas', component: AboutUsView, meta: { title: 'O nas' } },
 ];
+
+
 
 export function getRoutes() {
 	return routes;
